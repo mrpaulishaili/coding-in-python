4d0bfa96d84df719729f85755b293b0c96cfd696
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-10-09 17:59:08 +0100

    ..

--- panic.py
+++ panic.py
@@ -3,22 +3,23 @@
     "ON TAP" USING VARIOUS BUILT-IN FUNCTION FOR THE
     LIST DATA STRUCTURE.
 """
-
+# Lines of code given
 phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
 
+
+# Where the magic happens
 for i in range(4):
     plist.pop()
 
 plist.pop(0)
 plist.remove("'")
-
 plist.extend([plist.pop(), plist.pop()])
-
 plist.insert(2, plist.pop(3))
 
+# Lines of code given
 new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
