6d3bb438a0b91f6975c05fb3617c81d9b0816ac6
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-10-10 23:53:48 +0100

    ..

--- phonebook/README.md
+++ phonebook/README.md
@@ -1,6 +1,7 @@
-# PHONEBOOK PROGRAM
+# Phonebook Prgoram
 
 ## To create a phonebook program that:
+
 - [ ] Load a phonebook file
 - [ ] User can create new contact, {name: number}.
 - [ ] User can search for a name or number in the phonebook file
@@ -10,10 +11,43 @@
 ## What can be learnt
 
 1. Define a function
-2. Create an input function
+2. Create an multi-line input function
 3. Print in multi-line strings format
 4. Close a function
 
+## Concepts used
+
+1. Multiple line strings format
+2. Input and print Functions
+3. Data Structures: Dictionary and dictionary methods(create empty dictionary, delete and update an item)
+4. User-defined Functions
+5. Control Flow statements(Iterative, Conditional and Transfer statement(break, return))
+
+## Pseudocode
+
+1. Define a function to welcome the user and provide options on how the phonebook works.
+   - [x] Define a function named 'welcome'
+   - [x] Create a variable using the input function and multi-line strings format inside the function.
+   - [x] Close the function
+2. Define the main function "phonebook"
+   - [x] Initiate a function name "phonebook"
+   - [x] Initiate an empty 'contact' dictionary to store values, inside the phonebook function block.
+3. Initiate a loop inside the function to continuously run the phonebook program.
+   - [x] Initiate a while loop to continuously run the phonebook program
+   
+4. Call 'welcome' function. Set the entry variable to welcome function.
+   - [x] Set entry variable to welcome function
+5. Create conditions for decision-making, for any option entered by the user.
+   - [ ] For the first entry, check if the contact dictionary is initially empty. If it is not empty, print the current contact list in the phonebook
+   - [ ] Otherwise, inform the user that the Phonebook is empty.
+6. Create decision-making for the second entry
+7. Create a decision for third entry
+8. Create a decision for the fourth entry
+9. Create a decision for the fifth entry. Break the program and exit.
+10. If entered entry is not an "entry" variable, display an error message.
+11. 🚀 PROGRAM COMPLETE.
+
 ## Resources
 
-- 📝 **Building a Simple Phonebook** 👉 [Here](https://mardiyyah.medium.com/building-a-simple-phonebook-learnpythonthroughprojects-series-10-af56d527f463)
+- 📝 **Building a Simple Phonebook**
+  👉 [Here](https://mardiyyah.medium.com/building-a-simple-phonebook-learnpythonthroughprojects-series-10-af56d527f463)
--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -7,3 +7,122 @@
         5. A particular Algorithm is Identified and used with huge
             consideration to it's Big O notation.
 """
+import time
+
+contact = {}
+
+contact_file = 'a://phonebook.txt'
+
+
+def contacts_file_content():
+    file_contents = open(contact_file, "r+").read()
+    return file_contents
+
+
+phonebook_options = {
+    1: "Display",
+    2: "Create",
+    3: "Search",
+    4: "Delete",
+    5: "Close"
+}
+
+
+# Define function to welcome user and provide options on how the phonebook works
+def welcome():
+    time.sleep(3)
+    #  Create an entry variable using the input function and multiple line strings format
+    entry = int(input("""
+                >>> 🤗 What would you like to do? <<<
+                
+                    1. Display      2. Create
+                    3. Search       4. Delete 
+                             5. Close
+                             
+                Enter your entry here (1,2,3, or 4): """))
+    #  Close function
+    return entry
+
+
+# Define display_contact function
+def display_contact():
+    #  If contact is not empty, print contacts
+
+    file_contents = contacts_file_content()
+    print(file_contents)
+    if bool(contact):
+        for name, number in contact.items():
+            print(name, '-->', number)
+    #  else inform that contact book is empty
+    else:
+        print('Phonebook is currently empty! Add a new contact to create one.')
+    return
+
+
+#  Define create_contact function
+def create_contact():
+    phone_number = input('Enter phone number: ')
+    contact_name = input('What would you want to save this contact as? "Firstname Lastname": ')
+
+    def save_contact():
+        contact.update({contact_name: phone_number})
+        file1 = open(contact_file, "a")
+        file1.write("{" + contact_name + ": " + phone_number + "}\n")
+        file1.close()
+        return
+
+    def validate_input():
+        # Check if contact doesn't exist, then add new contact to contacts
+        if contact_name not in contact:
+            save_contact()
+            display_contact()
+
+        # else inform that the contact already exists
+        else:
+            print('This contact already exists in phonebook.')
+            return
+
+    validate_input()
+
+
+#  Define search_contact function
+def search_contact():
+    contact_name = input('Enter contact name you wish to view')
+
+    if contact_name in contact:
+        print('The contact result:', contact_name, '-->', contact[contact_name])
+
+    else:
+        print('Not found!')
+
+
+#  Define delete_contact function
+def delete_contact():
+    ...
+
+
+# Define a function phonebook
+def phonebook():
+
+    # Initiate a while loop to continuously run the phonebook program
+    while True:
+        try:
+            #  Set entry variable to welcome function
+            entry = welcome()
+
+            #  Create conditions for decision-making, for any option entered by the user.
+            if entry == 1:
+                display_contact()
+            elif entry == 2:
+                create_contact()
+            elif entry == 3:
+                search_contact()
+            elif entry == 4:
+                delete_contact()
+            else:
+                print('Select a valid option')
+        except ValueError:
+            print('Select a valid option')
+
+
+phonebook()
