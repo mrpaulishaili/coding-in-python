6fd8da11bc4eb1813511e5a521974a550e05dce8
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-10-11 01:42:50 +0100

    Time Converter Program

--- time-converter/main.py
+++ time-converter/main.py
@@ -1,6 +1,18 @@
 """
-    ---   META-DATA   ---
+    ---   Meta Information   ---
     Time Converter Program
     Written in Python
     Developed by: Paul Ishaili C.
 """
+
+
+def TimeConverter():
+    pass
+
+
+def validate_input():
+    pass
+
+
+def execute_action():
+    pass
