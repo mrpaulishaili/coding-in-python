c034e0005141dfcb5f3ca19dcdf309a8d388dc98
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-10-12 05:26:23 +0100

    version 1.0.1

---
+++ my_modules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vls_search',
+    version='1.0.1',
+    description='The Head First Python Search Tools',
+    author='Paul Ishaili C.',
+    author_email='paulishaili@gmail.com',
+    url='https://mrpaulisahili.github.io/portforlio',
+    py_modules=['vls_search']
+)
